a37aa9fb171670e2aa526ec6cb56aa022eb4f155
Nizkovskii <97701131+Nizkovskii@users.noreply.github.com>
2022-01-29 22:03:08 +0300

    1.4

---
+++ HelloWorld/Условный оператор 1.4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    // условие треугольника — сложение двух сторон должно быть больше третьей. База 7 класса.
+    if (a + b > c && a + c > b && b + c > a) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+    return 0;
+}
+
